3ca16b0f8a0ddd3a5e606f5ac07542962c5dc729
christhopermarcelino <christhopermarcelino@gmail.com>
2022-10-25 15:14:39 +0700

    add script no 8, 9, 10, 11

---
+++ sss/init.sh
@@ -0,0 +1,7 @@
+apt-get update
+apt-get install lynx
+
+echo '10.11.3.3	wise.yyy.com' >> /etc/hosts
+echo '10.11.3.3	www.wise.yyy.com' >> /etc/hosts
+echo '10.11.3.3	eden.wise.yyy.com' >> /etc/hosts
+echo '10.11.3.3	www.eden.wise.yyy.com' >> /etc/hosts
